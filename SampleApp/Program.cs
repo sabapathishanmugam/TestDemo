eed348b26b7651fee1870a905f3dc24bbc20ecb9
sabapathishanmugam <sabapathi.shanmugam@aspiresys.com>
2026-01-29 06:57:51 +0000

    "Added initial SampleApp project files"

---
+++ SampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Welcome";
+
+        char[] eachChar = name.ToCharArray();
+        foreach (char c in eachChar)
+        {
+            Console.WriteLine(c);
+        }
+        Console.WriteLine(" ");
+    }
+}
